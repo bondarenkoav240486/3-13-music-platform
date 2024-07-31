301c53b84fac97a04d6550a07be871bec24746ba
Anton Bondarenko <bondarenko.av24@gmail.com>
2024-07-31 13:56:01 +0300

    add many ch in styles and background

--- pages/tracks/create.tsx
+++ pages/tracks/create.tsx
@@ -88,76 +88,80 @@ const Create = () => {
     }
 
     return (
+        <div className='AddSong'>
 
-        <MainLayout>
-            {loading && (
-                <div style={{
-                    position: 'fixed',
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                    height: '100%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    backgroundColor: 'rgba(255, 255, 255, 0.7)',
-                    zIndex: 9999
-                }}>
-                    <CircularProgress />
-                </div>
-            )}
-            
-            <StepWrapper activeStep={activeStep}>
-                {activeStep === 0 &&
-                    <Grid container direction={"column"} style={{ padding: 20 }}>
-                        <TextField
-                            {...name}
-                            style={{ marginTop: 10 }}
-                            label={"Назва пісні"}
-                        />
-                        <TextField
-                            {...artist}
-                            style={{ marginTop: 10 }}
-                            label={"Ім'я виконавця"}
-                        />
-                        <TextField
-                            {...text}
-                            style={{ marginTop: 10 }}
-                            label={"Слова до пісні"}
-                            multiline
-                            rows={3}
-                        />
-                    </Grid>
-                }
-                {activeStep === 1 &&
-                    <FileUpload setFile={setPicture} accept="image/*">
-                        <Button>Завантажити зображення</Button>
-                    </FileUpload>
-                }
-                {activeStep === 2 &&
-                    <FileUpload setFile={setAudio} accept="audio/*">
-                        <Button>Завантажити аудіо</Button>
-                    </FileUpload>
-                }
-            </StepWrapper>
-            <Grid container justifyContent='space-between'>
-                <Button disabled={activeStep === 0} onClick={back}>Назад</Button>
-                <Button onClick={next}>
-                    {/* Далі */}
-                    {loading ? <CircularProgress size={24} /> : 'Далі'}
-                </Button>
-            </Grid>
-            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-                <Alert onClose={handleClose} severity="error">
-                    {error}
-                </Alert>
-            </Snackbar>
-            <Snackbar open={success} autoHideDuration={6000} onClose={handleClose}>
-                <Alert onClose={handleClose} severity="success">
-                    Пісню успішно додано!
-                </Alert>
-            </Snackbar>
-        </MainLayout>
+            <MainLayout >
+                {loading && (
+                    <div style={{
+                        position: 'fixed',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+                        zIndex: 9999
+                    }}>
+                        <CircularProgress />
+                    </div>
+                )}
+
+                <StepWrapper activeStep={activeStep}>
+                    {activeStep === 0 &&
+                        <Grid container direction={"column"} style={{ padding: 20 }}>
+                            <TextField
+                                {...name}
+                                style={{ marginTop: 10 }}
+                                label={"Назва пісні"}
+                            />
+                            <TextField
+                                {...artist}
+                                style={{ marginTop: 10 }}
+                                label={"Ім'я виконавця"}
+                            />
+                            <TextField
+                                {...text}
+                                style={{ marginTop: 10 }}
+                                label={"Слова до пісні"}
+                                multiline
+                                rows={3}
+                            />
+                        </Grid>
+                    }
+                    {activeStep === 1 &&
+                        <FileUpload setFile={setPicture} accept="image/*">
+                            <Button>Завантажити зображення</Button>
+                        </FileUpload>
+                    }
+                    {activeStep === 2 &&
+                        <FileUpload setFile={setAudio} accept="audio/*">
+                            <Button>Завантажити аудіо</Button>
+                        </FileUpload>
+                    }
+                </StepWrapper>
+                <Grid container justifyContent='space-between'>
+                    <Button disabled={activeStep === 0} onClick={back}>Назад</Button>
+                    <Button onClick={next}>
+                        {/* Далі */}
+                        {loading ? <CircularProgress size={24} /> : 'Далі'}
+                    </Button>
+                </Grid>
+                <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                    <Alert onClose={handleClose} severity="error">
+                        {error}
+                    </Alert>
+                </Snackbar>
+                <Snackbar open={success} autoHideDuration={6000} onClose={handleClose}>
+                    <Alert onClose={handleClose} severity="success">
+                        Пісню успішно додано!
+                    </Alert>
+                </Snackbar>
+
+            </MainLayout>
+        </div>
+
     );
 };
 
